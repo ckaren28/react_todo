b0380e9f0da7711d03d2a0e61ef03c8c254c9491
Karen Chin <c.karen28@gmail.com>
2016-11-08 16:35:50 -0700

    remove, edit actions

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
 import React from "react";
-import ToDosList from "./todosList";
 import CreateTodo from "./createTodos";
+import ToDosList from "./todosList";
 
 const todos = [
     {
@@ -23,9 +23,43 @@ export default class App extends React.Component{
     render() {
         return (
             <div>
-            <h1> React todo app</h1>
-            < ToDosList todos= {this.state.todos} />
+                <h1>React ToDos App</h1>
+                <CreateTodo todos={this.state.todos} createTask={this.createTask.bind(this)} />
+                < ToDosList
+                    todos = {this.state.todos}
+                    toggleTask={this.toggleTask.bind(this)}
+                    saveTask={this.saveTask.bind(this)}
+                    deleteTask={this.deleteTask.bind(this)}
+                />
             </div>
         );
     }
+
+    createTask(task){
+        this.state.todos({
+            task,
+            isCompleted: false
+        });
+        this.setState({todos: this.state.todos });
+    }
+
+    toggleTask(task) {
+        const foundTodo = _.find(this.state.todos, todo => todo.task === task);
+        foundTodo.isCompleted = !foundTodo.isCompleted;
+        this.setState({ todos: this.state.todos });
+    }
+    saveTask(oldTask, newTask) {
+        const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
+        foundTodo.task = newTask;
+        this.setState({ todos: this.state.todos });
+    }
+
+    deleteTask(taskToDelete) {
+        _.remove(this.state.todos, todo => todo.task === taskToDelete);
+        this.setState({ todos: this.state.todos });
+    }
+
+
+
+
 }
--- src/components/todosListItem.js
+++ src/components/todosListItem.js
@@ -16,26 +16,51 @@ export default class ToDosListItem extends React.Component{
         if (this.state.isEditing) {
             return(
                 <td>
-                <button >Save</button>
-                <button onCLick={this.onCancelClick.bind(this)}>Cancel</button>
+                    <button >Save</button>
+                    <button onClick={this.onCancelClick.bind(this)}>Cancel</button>
                 </td>
             );
         }
         return (
             <td>
-            <button onClick={this.onEditClick.bind(this)}>Edit</button>
-            <button>Delete</button>
+                <button onClick={this.onEditClick.bind(this)}>Edit</button>
+                <button>Delete</button>
             </td>
-        )
+        );
+    }
+
+    renderTaskSection(){
+        const { task, isCompleted } = this.props;
+
+        const taskStyle = {
+            color: isCompleted ? 'green' : 'red',
+            cursor: 'pointer'
+        };
+
+        if (this.state.isEditing) {
+            return(
+                <td>
+                    <form onSubmit={this.onSaveClick.bind(this)}>
+                        <input type="text" defaultValue={task} ref="editInput" />
+                    </form>
+                </td>
+            );
+        }
+        return (
+            <td style={taskStyle}
+                onClick={this.props.toggleTask.bind(this, task)}
+            >
+                {task}
+            </td>
+        );
     }
 
     render() {
         return (
             <tr>
-                <td>{this.props.tasks}</td>
-                <td>
-                    {this.renderActionsSection()}
-                </td>
+                {this.renderTaskSection()}
+                {this.renderActionsSection()}
+
             </tr>
         );
     }
@@ -46,4 +71,12 @@ export default class ToDosListItem extends React.Component{
     onCancelClick(){
         this.setState({ isEditing: false})
     }
+    onSaveClick(event) {
+       event.preventDefault();
+
+       const oldTask = this.props.task;
+       const newTask = this.refs.editInput.value;
+       this.props.saveTask(oldTask, newTask);
+       this.setState({ isEditing: false });
+   }
 }
